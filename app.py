afadf2ede64a167a6dc37548a3f3e543157ba50f
EC2 Default User <ec2-user@ip-172-31-26-7.ap-northeast-1.compute.internal>
2019-09-04 07:23:24 +0000

    commit

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ from boto3.dynamodb.conditions import Key
 
 app = Chalice(app_name='serverless-handson01')
 table = boto3.resource('dynamodb', region_name=REGION_NAME).Table('serverless-handson01-table')
+SUCCESS_CD = "0"
+ERROR_CD = "9"
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET'], cors=cors_config)
 def index():
     return 'test'
 
@@ -15,20 +17,52 @@ def index():
 def test1():
     return 'test1'
 
-@app.route('/workshop', methods=['GET'])
+
+#########################################################
+## 検索処理
+#########################################################
+@app.route('/workshop', methods=['GET'], cors=cors_config)
 def get_workshop():
+    
+    #検索パラメータの取得
     request = app.current_request.query_params
     if 'ID' in request:
+        id = request['ID']
+    
+    #検索処理
+    if id != '':
+        #検索条件が指定されている場合
         queryData = table.query(KeyConditionExpression = Key('ID').eq(request['ID']))
-        return queryData['Items']
     else:
-        return 'parameter error'
+        #検索条件の指定がない場合
+        queryData = table.scan()
+    
+    #検索データの返却 
+    if queryData['Items'] == []:
+        return {"result": ERROR_CD, "data": "検索結果が 0件 でした。"}
+    return {"result": SUCCESS_CD, "data": queryData['Items']}
 
-@app.route('/workshop', methods=['PUT'])
+
+#########################################################
+## 登録処理
+#########################################################
+@app.route('/workshop', methods=['PUT'], cors=cors_config)
 def put_workshop():
+    
+    #登録パラメータの取得
     request = app.current_request.json_body
-    if 'ID' in request:
-        queryData = table.query(KeyConditionExpression = Key('ID').eq(request['ID']))
-        return queryData['Items']
-    else:
-        return 'parameter error'
+    if request['ID'] == '':
+        return {"result": ERROR_CD, "data": "社員番号は必須です。"}
+        
+    #データ登録処理 
+    try:
+        table.put_item(
+            Item={
+            'ID': request['ID'],
+            'Name': request['Name']
+            },
+            ConditionExpression='attribute_not_exists(ID)'
+        )
+        return {"result": SUCCESS_CD, "data": "登録しました。"}
+    except Exception as e:
+        return {'result':ERROR_CD, 'data': 'すでに登録されています。'}
